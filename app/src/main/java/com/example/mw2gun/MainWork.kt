62fc8bed54e6aee08d6015219bfaefe31d9a7853
AliZulnoor <90544813+AliZulnoor@users.noreply.github.com>
2022-12-12 19:06:47 +0400

    loading screen

--- app/src/main/java/com/example/mw2gun/MainWork.kt
+++ app/src/main/java/com/example/mw2gun/MainWork.kt
@@ -1,21 +1,26 @@
 package com.example.mw2gun
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.view.View
 import kotlinx.android.synthetic.main.activity_main_work.*
+import kotlinx.android.synthetic.main.popup.*
 
 
- class MainWork : AppCompatActivity() {
+class MainWork : AppCompatActivity() {
      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_work)
-
-
-
+         val myDialoug = Dialog(this)
+         myDialoug.setContentView(R.layout.popup)
+         myDialoug.setCancelable(true)
+         myDialoug.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+         myDialoug.show()
          var play= MediaPlayer.create(this, R.raw.change);
         findViewById<ImageView>(R.id.gun);
         findViewById<Button>(R.id.goldButton);
